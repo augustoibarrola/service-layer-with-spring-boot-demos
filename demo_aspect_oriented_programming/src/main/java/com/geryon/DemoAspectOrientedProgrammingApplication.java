c2ed599867cb1de3dc38ecc716ec874a83feb891
augustoibarrola <augusto.t.ibarrola@gmail.com>
2022-11-18 11:54:56 -0500

    development

---
+++ demo_aspect_oriented_programming/src/main/java/com/geryon/DemoAspectOrientedProgrammingApplication.java
@@ -0,0 +1,13 @@
+package com.geryon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoAspectOrientedProgrammingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoAspectOrientedProgrammingApplication.class, args);
+	}
+
+}
---
+++ demo_aspect_oriented_programming/src/test/java/com/geryon/DemoAspectOrientedProgrammingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.geryon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoAspectOrientedProgrammingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
